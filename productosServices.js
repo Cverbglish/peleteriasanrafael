6e794852d74fa0926b947efad3a314bc8280ef77
Cverbglish <156864955+Cverbglish@users.noreply.github.com>
2024-06-11 09:44:16 -0600

    Update productosServices.js

--- productosServices.js
+++ productosServices.js
@@ -1,16 +1,5 @@
-function getProds(){
-   return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-          resolve(JSON.parse(xhr.responseText));
-        } else {
-          reject(xhr.statusText);
-        }
-      }
-    };
-    xhr.open("GET", "https://peleteriasanrafael.vercel.app/api/productos", true); // Asegúrate de que esta URL es correcta
-    xhr.send();
-  });
+async function getProds(){
+    const res = await fetch("https://peleteriasanrafael.vercel.app:4000/productos");
+    const resJson = await res.json();
+    return resJson;
   }
