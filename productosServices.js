215c5898ffd7af0d89205c0d9cc7e118d952c118
Cverbglish <156864955+Cverbglish@users.noreply.github.com>
2024-06-10 22:09:24 -0600

    Update productosServices.js

--- productosServices.js
+++ productosServices.js
@@ -1,5 +1,5 @@
 async function getProds(){
-    const res = await fetch("http://localhost:8080/productos");
+    const res = await fetch("https://peleteriasanrafaelventas.cleverapps.io/:8080/productos");
     const resJson = await res.json();
     return resJson;
   }
